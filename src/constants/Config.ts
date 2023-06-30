67b951e4c792955696bbd3d7a238ff272be0ad74
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-06-30 15:12:29 +0700

    first commit!

---
+++ src/constants/Config.ts
@@ -0,0 +1,16 @@
+﻿import Phaser from 'phaser'
+
+
+const GAME_CONFIG: Phaser.Types.Core.GameConfig = {
+    parent: 'game',
+    scale: {
+        mode: Phaser.Scale.RESIZE,
+    },
+    scene: [],
+    backgroundColor: 0xffffff,
+    physics: {
+        default: 'arcade',
+    },
+}
+
+export default GAME_CONFIG
---
+++ src/game.ts
@@ -0,0 +1,13 @@
+import GAME_CONFIG from './constants/Config'
+import Phaser from 'phaser'
+
+export class Game extends Phaser.Game
+{
+    constructor(config: Phaser.Types.Core.GameConfig) {
+        super(config)
+    }
+}
+
+window.addEventListener('load', () => {
+    const game = new Game(GAME_CONFIG)
+})
