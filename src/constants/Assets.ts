663ac030c64087490f0da4a0804515326fc61b12
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-07-03 10:58:54 +0700

    feat: tilemap asset

--- src/constants/Assets.ts
+++ src/constants/Assets.ts
@@ -9,13 +9,19 @@
 
         export enum Sprite
         {
-            DEFAULT = 'a'
+            CIRCLE = 'key.sprite.circle',
+            SQUARE = 'key.sprite.square',
+            GRADIENT_TOP_TO_BOTTOM = 'key.sprite.gradient-top-to-bottom',
+            KENNEY_DEFAULT_TILESET = 'key.sprite.kenney-default-tileset',
         }
     }
 
     export const FileLookUp = {
         [Assets.Key.Audio.DEFAULT]: '',
-        [Assets.Key.Sprite.DEFAULT]: '',
+        [Assets.Key.Sprite.CIRCLE]: './assets/circle.png',
+        [Assets.Key.Sprite.SQUARE]: './assets/square.png',
+        [Assets.Key.Sprite.GRADIENT_TOP_TO_BOTTOM]: './assets/gradient-top-to-bottom.png',
+        [Assets.Key.Sprite.KENNEY_DEFAULT_TILESET]: './assets/kenney-pixel-platformer.png',
     }
 }
 
