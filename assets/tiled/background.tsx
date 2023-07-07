cd20797e6a16f93b5037c9acc8251c00e94963de
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-07-07 18:15:08 +0700

    other: build

---
+++ assets/tiled/background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="background" tilewidth="24" tileheight="24" tilecount="12" columns="6">
+ <image source="../../../../Desktop/kenney_pixel-platformer(1)/Background/tilemap_packed.png" width="144" height="48"/>
+</tileset>
